911ef4082cf724352197833ca33988261b24956d
Marshal Wu <marshal.wu@gmail.com>
2012-05-10 12:30:28 +0800

    第一个版本，最简单的socketio使用

---
+++ app.js
@@ -0,0 +1,44 @@
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+    , routes = require('./routes')
+    , io = require('socket.io');
+
+var app = module.exports = express.createServer(),
+    io = io.listen(app);
+
+// Configuration
+
+app.configure(function () {
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function () {
+    app.use(express.errorHandler({ dumpExceptions:true, showStack:true }));
+});
+
+app.configure('production', function () {
+    app.use(express.errorHandler());
+});
+
+// Routes
+
+app.get('/', routes.index);
+
+app.listen(3000);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+
+io.sockets.on('connection', function (socket) {
+    console.log('connected');
+    socket.on('echo',function(data){
+        console.log('echo, user name: '+data.name);
+    });
+    socket.emit('hello','这里是测试服务器');
+});
