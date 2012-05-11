5b3561a838846ece79e1cc002dbe40d7d0fdf501
Marshal Wu <marshal.wu@gmail.com>
2012-05-11 11:42:49 +0800

    简单的测试，服务器端自动重连

--- app.js
+++ app.js
@@ -35,10 +35,14 @@ app.get('/', routes.index);
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 
-io.of('/echo').on('connection', function (socket) {
-    console.log('connected');
-    socket.on('echo', function (name,fn) {
-        console.log('收到来自'+name+'的消息。');
-        fn('好的'+name+'，收到回执！');
-    });
+//io.of('/echo').on('connection', function (socket) {
+//    console.log('connected');
+//    socket.on('echo', function (name,fn) {
+//        console.log('收到来自'+name+'的消息。');
+//        fn('好的'+name+'，收到回执！');
+//    });
+//});
+
+io.on('connection', function (socket) {
+    console.log('>>>>>>>connected');
 });
